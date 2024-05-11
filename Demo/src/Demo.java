71f729f10a9cd86da36593eea8998284142df821
nihal <nihalsinghparihar@gmail.com>
2024-05-11 11:09:08 +0530

    V2

--- Demo/src/Demo.java
+++ Demo/src/Demo.java
@@ -9,11 +9,24 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		String str="helloooo";
-		char [] ch=str.toCharArray();
+		/*
+		 * String str="helloooo"; char [] ch=str.toCharArray();
+		 * 
+		 * Map<Character, Integer> map=new HashMap<Character, Integer>(); for(char i:ch)
+		 * {
+		 * 
+		 * if(map.containsKey(i)) { int value=map.get(i); map.put(i, value+1); } else {
+		 * 
+		 * map.put(i, 1);
+		 * 
+		 * } } System.out.println(map);
+		 */
+		
+		int []arr= {2,2,3,4,5,5,5,6,6};
+		//char [] ch=str.toCharArray();
 	
-		Map<Character, Integer> map=new HashMap<Character, Integer>();
-		for(char i:ch)		{
+		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+		for(int i:arr)		{
 			
 			if(map.containsKey(i))
 			{
